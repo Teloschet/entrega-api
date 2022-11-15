bc2b9799502b7564a86cc1245dd0b518fba11984
Teloschet <chrisaleiro@gmail.com>
2022-11-15 17:39:21 -0300

    feat: create ClientController

---
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+import { CreateClientUseCase } from "./CreateClientUseCase";
+
+export class CreateClientController {
+  async handle(req: Request, res: Response) {
+    // Pega as requisições da rota
+    const { username, password } = req.body;
+
+    // Chama a classe do useCase que foi criado
+    const createClientUseCase = new CreateClientUseCase();
+    // Executa o useCase
+    const result = await createClientUseCase.execute({
+      username,
+      password
+    });
+
+    // Retorna os dados do useCase que foram cadastrados no banco via json
+    return res.json(result);
+  }
+}
