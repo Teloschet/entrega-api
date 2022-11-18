fd4cb17bdc8dc1a3b76f467657800607dc2d39e3
Teloschet <chrisaleiro@gmail.com>
2022-11-18 00:27:17 -0300

    feat(deliveryman)!: create service userCase

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliveryManUseCase.ts
@@ -0,0 +1,35 @@
+import { ICreateDeliveryman } from '../../interfaces/ICreateDeliveryman';
+
+import { prisma } from '@prisma/prismaClient';
+
+import { hash } from 'bcrypt';
+
+export class CreateDeliveryManUseCase {
+  async execute({ username, password }: ICreateDeliveryman) {
+    // Validar se o usuário existe
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: 'insensitive',
+        },
+      },
+    });
+
+    if (deliverymanExist) {
+      throw new Error('Deliveryman already exists');
+    }
+
+    // Criptografar a senha
+    const hashPassword = await hash(password, 10);
+
+    // Salvar o deliveryman
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    });
+
+    return deliveryman;
+  }
+}
