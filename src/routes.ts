1d6970ac60a75942b35c2b651f9d86e308264d6e
Teloschet <chrisaleiro@gmail.com>
2022-11-19 12:44:51 -0300

    feat(deliveryman): created routes from authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import { AuthenticateClientController } from '@accounts/useCases/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from '@clients/useCases/createClient/CreateClientController';
 import { CreateDeliveryManController } from '@deliveryman/useCases/createDeliveryman/CreateDeliveryManController';
+import { AuthenticateDeliveryManController } from '@accounts/useCases/authenticateDeliveryman/AuthenticateDeliveryManController';
 
 const routes = Router();
 
-const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliveryManController = new AuthenticateDeliveryManController();
+const createClientController = new CreateClientController();
 const createDeliveryManController = new CreateDeliveryManController();
 
-routes.post('/authenticate', authenticateClientController.handle);
+routes.post('/client/authenticate', authenticateClientController.handle);
+routes.post('/deliveryman/authenticate', authenticateDeliveryManController.handle);
 
 routes.post('/client/', createClientController.handle);
 routes.post('/deliveryman', createDeliveryManController.handle);
