9408ea1d7d4879cb1890ecfc2d658615a95281f0
Teloschet <chrisaleiro@gmail.com>
2022-11-28 23:45:03 -0300

    feat(routes)!: was created a `new route` from `findAllDeliveries`

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ import { ensureAuthenticateClient } from '@middlewares/ensureAuthenticateClient'
 import { FindAllWithoutEndDateController } from '@deliveries/useCases/findAllWithoutEndDate/FindAllWithoutEndDateController';
 import { ensureAuthenticateDeliveryman } from '@middlewares/ensureAuthenticateDeliveryman';
 import { UpdateDeliverymanController } from '@deliveries/useCases/updateDeliveryman/UpdateDeliverymanController';
+import { FindAllDeliveriesController } from '@clients/useCases/deliveries/FindAllDeliveriesController';
 
 const routes = Router();
 
@@ -19,6 +20,7 @@ const createDeliveryManController = new CreateDeliveryManController();
 const deliveryController = new CreateDeliveryController();
 const findAllDeliveriesWithoutEndDateController = new FindAllWithoutEndDateController();
 const updateDeliverymanController = new UpdateDeliverymanController();
+const findAllDeliveriesClient = new FindAllDeliveriesController();
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post('/deliveryman/authenticate', authenticateDeliveryManController.handle);
@@ -29,5 +31,7 @@ routes.post('/deliveryman', createDeliveryManController.handle);
 routes.post('/delivery', ensureAuthenticateClient, deliveryController.handle);
 routes.get('/delivery/available', ensureAuthenticateDeliveryman, findAllDeliveriesWithoutEndDateController.handle);
 routes.put('/delivery/updateDeliveryman/:id', ensureAuthenticateDeliveryman, updateDeliverymanController.handle);
+
+routes.get('/client/deliveries', ensureAuthenticateClient, findAllDeliveriesClient.handle);
 
 export { routes };
