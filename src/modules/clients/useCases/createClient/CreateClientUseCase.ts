33dcdf5d324aa2de812552ab9dbfdcac436ca59c
Teloschet <chrisaleiro@gmail.com>
2022-11-28 23:58:16 -0300

    fix(find)!: `equals` to `createCases` was fixed

--- src/modules/clients/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -9,6 +9,7 @@ export class CreateClientUseCase {
     const clientExist = await prisma.clients.findFirst({
       where: {
         username: {
+          equals: username,
           mode: 'insensitive',
         },
       },
