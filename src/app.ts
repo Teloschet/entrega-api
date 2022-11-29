b4e1ab6ef134077064d08a4041026aebf71dfdfa
Teloschet <chrisaleiro@gmail.com>
2022-11-28 22:05:09 -0300

    fix(app): orders of `call dependencies` was fixed

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import express from 'express';
 import 'express-async-errors';
+import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 
-import { ErrorMiddleware } from 'middlewares/ErrorMiddleware';
+import { ErrorMiddleware } from '@middlewares/ErrorMiddleware';
 import { routes } from './routes';
+
+import swaggerOutput from './docs/swagger_output.json';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(routes);
+app.use(morgan('dev', { skip: () => process.env.NODE_ENV === 'test' }));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
+app.use(routes);
 app.use(ErrorMiddleware);
 
 export { app };
